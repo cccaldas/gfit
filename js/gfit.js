c6ff5c4d73a09c740f7d24dc2eb992b8d8916064
Cristiano Caldas <eu@cccaldas.com>
2011-07-04 18:12:32 -0300

    first commit

---
+++ js/gfit.js
@@ -0,0 +1,53 @@
+var GFit = {};
+
+GFit.ready = function()
+{		
+	GFit.app = $("#" + GFit.target);
+	GFit.init();
+	GFit.resize();
+}
+
+GFit.init = function()
+{	
+	//alert(GFit.app.width());
+	GFit.width 	= GFit.app.width();
+	GFit.height	= GFit.app.height();
+	//alert("GFit.init");
+}
+
+GFit.resize = function()
+{
+	//caso a window.width for menor que o tamanho minimo
+	if($(window).width() < GFit.width)
+	{
+		GFit.app.width(GFit.width);
+		$("body").css("overflow-x", "scroll");
+	}
+	else
+	{
+		GFit.app.width("100%");
+		$("body").css("overflow-x", "hidden");
+	}
+	
+	//caso a window.height for menor que o tamanho minimo
+	if($(window).height() < GFit.height)
+	{
+		GFit.app.height(GFit.height);
+		$("body").css("overflow-y", "scroll");
+	}
+	else
+	{
+		GFit.app.height("100%");
+		$("body").css("overflow-y", "hidden");
+	}
+	
+	$("#lbl_status").text(GFit.app.width() + ", " + GFit.app.height());
+}
+
+GFit.fit = function(target)
+{
+	GFit.target = target;
+	$(document).ready(GFit.ready);
+	$(window).resize(GFit.resize);
+}
+
